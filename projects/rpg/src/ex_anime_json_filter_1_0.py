c69f8edceeb6989efc74ac856ef56ff5ef6aa7d0
japinol <japinol@outlook.com>
2022-03-20 23:21:43 +0100

    [IMP] ex_anime_json_filter_1_0: Minor improvements.

--- projects/rpg/src/ex_anime_json_filter_1_0.py
+++ projects/rpg/src/ex_anime_json_filter_1_0.py
@@ -52,27 +52,33 @@ def get_wanted_columns(anime):
     return {k: v for k, v in anime.items() if k in ANIME_COLUMNS_WANTED}
 
 
-file_name = os.path.join('..', 'res', 'data', 'anime-offline-database.json')
+def main():
+    file_name = os.path.join('..', 'res', 'data', 'anime-offline-database.json')
 
-start_time = time.time()
+    start_time = time.time()
 
-animes = []
-with open(file_name, encoding='UTF8') as fin:
-    data = json.load(fin)
-    for row in data['data']:
-        row.update({
-            'year': row['animeSeason'].get('year', 0) or 0
-            })
-        if not is_a_wanted_anime(row):
-            continue
-        row = get_wanted_columns(row)
-        animes.append(row)
+    animes = []
+    with open(file_name, encoding='UTF8') as fin:
+        data = json.load(fin)
+        for row in data['data']:
+            row.update({
+                'year': row['animeSeason'].get('year', 0) or 0
+                })
+            if not is_a_wanted_anime(row):
+                continue
+            row = get_wanted_columns(row)
+            animes.append(row)
 
-animes.sort(key=lambda x: x['year'])
-for anime in animes:
-    print('\n' * 3, '-' * 40, '\n')
-    pprint(anime)
+    animes.sort(key=lambda x: x['year'])
+    for anime in animes:
+        print('\n' * 3, '-' * 40, '\n')
+        pprint(anime)
 
-print('\n\n', '-' * 40)
-print(f"Animes: {len(animes)}")
-print(f'\nt: {time.time() - start_time:.{8}f} s')
+    print('-' * 40)
+    print(f"Animes: {len(animes)}")
+    print(f'\nt: {time.time() - start_time:.{8}f} s')
+    print('-' * 15)
+
+
+if __name__ == "__main__":
+    main()
