a31ab64f861a7e37685582a9fb92ac58f86295d9
japinol <japinol@outlook.com>
2022-05-02 23:53:42 +0200

    [IMP] gui_readiness_to_rock: Add new feature: tkinter gui example, readiness to rock.

---
+++ projects/rpg/src/gui_readiness_to_rock.py
@@ -0,0 +1,20 @@
+from tkinter import messagebox
+
+
+def main():
+    are_you_ready = messagebox.askyesno(
+        title='Rock readiness',
+        message='Are you ready to rock?',
+        detail='Click Yes if you are awesome'
+    )
+
+    if are_you_ready:
+        res = 'Ok. You are ready to rock.'
+    else:
+        res = "Don't worry. You will be ready to rock soon enough."
+
+    print(res)
+
+
+if __name__ == "__main__":
+    main()
