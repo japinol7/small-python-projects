88a786ac1e7ee9acf2b50797225a9a96cecd3c5e
japinol <japinol@outlook.com>
2022-06-24 09:31:44 +0200

    [IMP] mazes: Clean code.

--- projects/mazes/src/ex_maze_01.py
+++ projects/mazes/src/ex_maze_01.py
@@ -6,16 +6,18 @@ from config.config import log
 from utils.time_it import time_it
 from config.config import LOG_START_APP_MSG, LOG_END_APP_MSG
 from solver import calc_dfs
-from controller.controller import create_maze, solve_maze
+from controller.controller import MazeController
 
 
 def main():
     log.info(LOG_START_APP_MSG)
+
+    controller = MazeController()
     maze_name = 'maze_01'
-    maze = time_it(create_maze, name=maze_name, load_maze=True)
+    maze = time_it(controller.create_maze, name=maze_name, load_maze=True)
     print(maze)
 
-    solution_node = time_it(solve_maze, maze=maze, calc_solver=calc_dfs, save_maze=True)
+    solution_node = time_it(controller.solve_maze, maze=maze, calc_solver=calc_dfs, save_maze=True)
     if solution_node:
         print(maze)
     log.info(LOG_END_APP_MSG)
