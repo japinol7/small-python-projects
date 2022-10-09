3eb562b4228cd12236540b55b1bf5b21ea574c1a
japinol <japinol@outlook.com>
2022-10-09 17:11:29 +0200

    [IMP] math: Add fibonacci calculations.

---
+++ projects/math/src/fibonacci/__init__.py
@@ -0,0 +1,6 @@
+"""Package fibonacci."""
+__author__ = 'Joan A. Pinol  (japinol)'
+__all__ = ['fib_seq1_ite', 'fib_seq2_ite', 'fib_seq3_ite_gen', 'fib_sum1_ite',
+           'fib_sum2_ite', 'fib1_rec', 'fib2_ite', 'fib3a_rec_mem_list', 'fib3b_rec_mem_arr',
+           'fib4_rec_mem_dic', 'fib5_rec_mem', 'fib6_rec_mem', 'time_it', 'time_it_dec',
+           ]
