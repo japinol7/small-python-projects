03256ffefc180038adb841c38d9d5f65811eb802
japinol <japinol@outlook.com>
2022-10-20 22:50:34 +0200

    [IMP] various: Some improvements.

--- projects/various/src/binary_search_st_lib.py
+++ projects/various/src/binary_search_st_lib.py
@@ -3,16 +3,16 @@ import bisect
 from time_it.time_it import time_it
 
 
-def binary_search(nums, item):
+def binary_search_position_to_insert(nums, item):
     res = bisect.bisect_left(nums, item)
     return res
 
 
 def main():
-    input_item_to_find, input_nums = 3, [2, 1, 5, 1, 2, 2, 2]
-    nums = sorted(input_nums)
+    input_item_to_find, input_nums = 3, [2, 1, 5, 1, 2, 2, 2, 7]
+    input_nums = sorted(input_nums)
     print(input_nums)
-    result = time_it(binary_search, input_nums, input_item_to_find)
+    result = time_it(binary_search_position_to_insert, input_nums, input_item_to_find)
     print(f'Res: {result}')
 
 
