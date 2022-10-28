08ef3039e346432e7c00de1f6eae6ff75540eae3
japinol <japinol@outlook.com>
2022-10-28 23:25:29 +0200

    [IMP] roman_numerals: Some improvements.

---
+++ projects/roman_numerals/test_ex_01_number_to_roman_numeral_alt.py
@@ -0,0 +1,14 @@
+import pytest
+
+from ex_01_number_to_roman_numeral_alt import convert_to_roman_numeral
+
+
+@pytest.mark.parametrize("arabic_num, roman_numeral_num", [
+    (23, 'XXIII'), (884, 'DCCCLXXXIV'), (1995, 'MCMXCV'),
+    (999, 'CMXCIX'), (36, 'XXXVI'), (3017, 'MMMXVII'),
+    (994, 'CMXCIV'), (4978, 'MMMMCMLXXVIII'), (4999, 'MMMMCMXCIX'),
+    (0, ''), (52000, ''), ])
+def test_computer_player_move__one_cell_left(arabic_num, roman_numeral_num):
+    result = convert_to_roman_numeral(arabic_num)
+    expected = roman_numeral_num
+    assert result == expected
