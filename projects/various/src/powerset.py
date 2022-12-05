863d4ac7436318ddc1574692742961527ce8ca02
japinol <japinol@outlook.com>
2022-12-06 00:47:43 +0100

    [IMP] various: Improve project: powerset.

--- projects/various/src/powerset.py
+++ projects/various/src/powerset.py
@@ -1,14 +1,15 @@
 from itertools import chain, combinations
 
 
-def powerset(iterable, min_elements=0, max_elements=None):
-    """Example:
+def powerset(iterable, min_items=0, max_items=None):
+    """Examples:
     powerset([1, 2, 3]) --> [(), (1,), (2,), (3,), (1, 2), (1, 3), (2, 3), (1, 2, 3)]
+    powerset([1, 2, 3], min_items=2, max_items=2) --> [(1, 2), (1, 3), (2, 3)]
     """
     s = list(iterable)
-    if not max_elements:
-        max_elements = len(s)
-    return chain.from_iterable(combinations(s, r) for r in range(min_elements, max_elements + 1))
+    if not max_items:
+        max_items = len(s)
+    return chain.from_iterable(combinations(s, r) for r in range(min_items, max_items + 1))
 
 
 if __name__ == '__main__':
