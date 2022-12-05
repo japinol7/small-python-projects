ee0c4613a6e3d5090c5765d333abb70ce5d93cc9
japinol <japinol@outlook.com>
2022-12-06 00:41:42 +0100

    [IMP] various: Improve project: powerset.

--- projects/various/src/powerset.py
+++ projects/various/src/powerset.py
@@ -3,7 +3,7 @@ from itertools import chain, combinations
 
 def powerset(iterable, min_elements=0, max_elements=None):
     """Example:
-    powerset([1, 2, 3], 0) --> [(), (1,), (2,), (3,), (1, 2), (1, 3), (2, 3), (1, 2, 3)]
+    powerset([1, 2, 3]) --> [(), (1,), (2,), (3,), (1, 2), (1, 3), (2, 3), (1, 2, 3)]
     """
     s = list(iterable)
     if not max_elements:
@@ -12,5 +12,5 @@ def powerset(iterable, min_elements=0, max_elements=None):
 
 
 if __name__ == '__main__':
-    res = powerset([1, 2, 3], 0)
+    res = powerset([1, 2, 3])
     print(list(res))
