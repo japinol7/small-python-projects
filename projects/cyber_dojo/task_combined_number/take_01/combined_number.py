bd47cae8b41979341ad7ec2f4cb34d9ca6b777dc
japinol <japinol@outlook.com>
2023-01-11 00:13:17 +0100

    [IMP] cyber_dojo: Add project: task_combined_number.

---
+++ projects/cyber_dojo/task_combined_number/take_01/combined_number.py
@@ -0,0 +1,15 @@
+class CombinedNumber:
+    @staticmethod
+    def combined_number(nums):
+        if not nums:
+            return ''
+
+        res = [str(x) for x in nums]
+
+        res_len = len(res)
+        for i in range(res_len):
+            for j in range(res_len):
+                if res[i] + res[j] > res[j] + res[i]:
+                    res[i], res[j] = res[j], res[i]
+
+        return ''.join(res)
