4ce14dfbd9a9f216c39e394e34c574adab1368c0
japinol <japinol@outlook.com>
2023-01-30 20:08:38 +0100

    [IMP] cyber_dojo: Add project: task_vending_machine.

--- projects/cyber_dojo/task_vending_machine/take_01/test_vending_machine.py
+++ projects/cyber_dojo/task_vending_machine/take_01/test_vending_machine.py
@@ -14,3 +14,56 @@ class TestVendingMachine:
             'cola': {'name': 'cola', 'price': 1, 'qty': 2}
             }
         assert result == expected
+
+    def test_add_items__some_exist(self, stock_items):
+        vending_machine = VendingMachine()
+        initial_items = [
+            {'name': 'candy', 'qty': 1, 'price': 0.65},
+            {'name': 'cola', 'qty': 10, 'price': 0.65},
+            {'name': 'sprite', 'qty': 2, 'price': 0.70},
+            ]
+        vending_machine.add_items(initial_items)
+        vending_machine.add_items(stock_items)
+        result = vending_machine.items
+        expected = {
+            'candy': {'name': 'candy', 'price': 0.65, 'qty': 5},
+            'chips': {'name': 'chips', 'price': 0.5, 'qty': 1},
+            'cola': {'name': 'cola', 'price': 1, 'qty': 12},
+            'sprite': {'name': 'sprite', 'qty': 2, 'price': 0.70},
+            }
+        assert result == expected
+
+    @pytest.mark.parametrize('item, expected', [
+        (('chips', 1), (True, 0.5)),
+        (('cola', 2), (True, 1)),
+        ])
+    def test_process_order(self, stock_items, item, expected):
+        vending_machine = VendingMachine()
+        vending_machine.add_items(stock_items)
+        result = vending_machine.process_order(*item)
+        assert result == expected
+
+    @pytest.mark.parametrize('item, expected', [
+        (('chips', 1), 0),
+        (('candy', 2), 3),
+        ])
+    def test_after_process_order_stock_will_be_updated(self, stock_items, item, expected):
+        vending_machine = VendingMachine()
+        vending_machine.add_items(stock_items)
+        vending_machine.process_order(*item)
+        result = vending_machine.get_item_qty(item[0])
+        assert result == expected
+
+    def test_process_order__not_enough_stock(self, stock_items):
+        items_n_expected = [
+            ({'name': 'chips', 'price': 0.5}, 'SOLD OUT'),
+            ({'name': 'cola', 'price': 1}, 1),
+            ({'name': 'cola', 'price': 1}, 'SOLD OUT'),
+            ]
+        vending_machine = VendingMachine()
+        vending_machine.add_items(stock_items)
+
+        for item, expected in items_n_expected:
+            vending_machine.process_order(*item.values())
+            result = vending_machine.get_item_qty(item['name'])
+            assert result == expected
