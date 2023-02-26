60765d1e8660b29a20e3260789bdcb6814dec9ec
japinol <japinol@outlook.com>
2023-02-26 15:59:52 +0100

    [IMP] various: Improve project: print_diamond_in_square.

--- projects/various/src/print_diamond_in_square.py
+++ projects/various/src/print_diamond_in_square.py
@@ -5,7 +5,7 @@ CELL_FILL_PRETTY = f'{CELL_FILL}{CELL_WHITE_SPACE}'
 CELL_O_PRETTY = f'{CELL_O}{CELL_WHITE_SPACE}'
 
 
-def print_diamond_in_square(size):
+def get_diamond_in_square(size):
     if size % 2 == 0:
         print("User Error. You must supply an odd number, "
               f" but you provided: {size} !")
@@ -21,10 +21,14 @@ def print_diamond_in_square(size):
         column_pos_1 += 1 if i < half_size else -1
         column_pos_2 -= 1 if i < half_size + 1 else -1
         row[column_pos_2] = CELL_O
+    return grid
 
+
+def print_diamond_in_square(grid):
     for row in grid:
         print(''.join(row).replace(CELL_FILL, CELL_FILL_PRETTY).replace(CELL_O, CELL_O_PRETTY))
 
 
 if __name__ == '__main__':
-    print_diamond_in_square(15)
+    grid = get_diamond_in_square(15)
+    print_diamond_in_square(grid)
