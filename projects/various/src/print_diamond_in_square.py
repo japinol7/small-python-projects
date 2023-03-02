0ab84c3480f7696647ca1b5ebbcdbc89d3bbd979
japinol <japinol@outlook.com>
2023-03-02 19:35:15 +0100

    [IMP] various: Improve project: print_diamond_in_square.

--- projects/various/src/print_diamond_in_square.py
+++ projects/various/src/print_diamond_in_square.py
@@ -1,8 +1,8 @@
 CELL_FILL = '.'
-CELL_O = 'O'
+CELL_X = 'X'
 CELL_WHITE_SPACE = '  '
 CELL_FILL_PRETTY = f'{CELL_FILL}{CELL_WHITE_SPACE}'
-CELL_O_PRETTY = f'{CELL_O}{CELL_WHITE_SPACE}'
+CELL_O_PRETTY = f'{CELL_X}{CELL_WHITE_SPACE}'
 
 
 def get_diamond_in_square_grid(size):
@@ -17,16 +17,16 @@ def get_diamond_in_square_grid(size):
     column_pos_1 = half_size
     column_pos_2 = half_size + 1
     for i, row in enumerate(grid):
-        row[column_pos_1] = CELL_O
+        row[column_pos_1] = CELL_X
         column_pos_1 += 1 if i < half_size else -1
         column_pos_2 -= 1 if i < half_size + 1 else -1
-        row[column_pos_2] = CELL_O
+        row[column_pos_2] = CELL_X
     return grid
 
 
 def print_diamond_in_square_grid(grid):
     for row in grid:
-        print(''.join(row).replace(CELL_FILL, CELL_FILL_PRETTY).replace(CELL_O, CELL_O_PRETTY))
+        print(''.join(row).replace(CELL_FILL, CELL_FILL_PRETTY).replace(CELL_X, CELL_O_PRETTY))
 
 
 def main():
