0c5bae92d776f5b0cc1cacba60b6f58978ad8470
japinol <japinol@outlook.com>
2023-10-30 09:43:32 +0100

    [IMP] cyber_dojo: Improve project: closest_to_zero.

--- projects/cyber_dojo/task_closest_to_zero/take_01/closest_to_zero.py
+++ projects/cyber_dojo/task_closest_to_zero/take_01/closest_to_zero.py
@@ -1,4 +1,4 @@
-from collections.abc import Generator, Iterable, Sequence
+from collections.abc import Iterable
 import math
 
 
@@ -6,20 +6,7 @@ def get_number_closest_to_zero(input_vals: Iterable[int]) -> int:
     if not input_vals:
         return 0
 
-    if not isinstance(input_vals, Iterable):
-        raise TypeError(f"Input_vals parameter must be an iterable.")
+    min_positive = min((x for x in input_vals if x >= 0), default=math.inf)
+    min_negative = min((x for x in input_vals if x < 0), default=-math.inf)
 
-    if isinstance(input_vals, Sequence) and len(input_vals) == 1:
-        return input_vals[0]
-
-    if isinstance(input_vals, Generator):
-        input_vals = list(input_vals)
-
-    left_zero_vals = list(filter(lambda x: x < 0, input_vals))
-    min_left_val = -min(left_zero_vals) if left_zero_vals else math.inf
-
-    right_zero_vals = list(filter(lambda x: x >= 0, input_vals))
-    min_right_val = min(right_zero_vals) if right_zero_vals else math.inf
-
-    are_equal = min_right_val == min_left_val
-    return min_right_val if are_equal else min(min_left_val, min_right_val)
+    return min_positive if min_positive < -min_negative else min_negative
