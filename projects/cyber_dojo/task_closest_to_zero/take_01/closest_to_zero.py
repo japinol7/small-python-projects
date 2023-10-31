fc8b0a6ca8b0fda7e83b3127532d7f305fa8dd5f
japinol <japinol@outlook.com>
2023-10-31 02:04:31 +0100

    [IMP] cyber_dojo: Improve project: closest_to_zero.

--- projects/cyber_dojo/task_closest_to_zero/take_01/closest_to_zero.py
+++ projects/cyber_dojo/task_closest_to_zero/take_01/closest_to_zero.py
@@ -6,7 +6,7 @@ def get_number_closest_to_zero(input_vals: Iterable[int]) -> int:
     if not input_vals:
         return 0
 
-    min_positive = min((x for x in input_vals if x >= 0), default=math.inf)
-    min_negative = min((x for x in input_vals if x < 0), default=-math.inf)
+    min_positive = min((x for x in input_vals if x >= 0), default=int(math.inf))
+    min_negative = min((x for x in input_vals if x < 0), default=-int(math.inf))
 
     return min_positive if min_positive < -min_negative else min_negative
