f7d09768f71fe8150754e35ca4f704a10a73e2cf
japinol <japinol@outlook.com>
2023-10-31 02:26:33 +0100

    [IMP] cyber_dojo: Improve project: closest_to_zero.

--- projects/cyber_dojo/task_closest_to_zero/take_01/closest_to_zero.py
+++ projects/cyber_dojo/task_closest_to_zero/take_01/closest_to_zero.py
@@ -2,11 +2,14 @@ from collections.abc import Iterable
 import math
 
 
-def get_number_closest_to_zero(input_vals: Iterable[int]) -> int:
-    if not input_vals:
+def get_number_closest_to_zero(nums: Iterable[int]) -> int | float:
+    if not nums:
         return 0
 
-    min_positive = min((x for x in input_vals if x >= 0), default=int(math.inf))
-    min_negative = min((x for x in input_vals if x < 0), default=-int(math.inf))
+    if not isinstance(nums, Iterable):
+        raise TypeError("Input_vals must be an iterable.")
+
+    min_positive = min((x for x in nums if x >= 0), default=math.inf)
+    min_negative = min((x for x in nums if x < 0), default=-math.inf)
 
     return min_positive if min_positive < -min_negative else min_negative
--- projects/cyber_dojo/task_closest_to_zero/take_02/closest_to_zero.py
+++ projects/cyber_dojo/task_closest_to_zero/take_02/closest_to_zero.py
@@ -2,23 +2,23 @@ from collections.abc import Generator, Iterable, Sequence
 import math
 
 
-def get_number_closest_to_zero(input_vals: Iterable[int]) -> int:
-    if not input_vals:
+def get_number_closest_to_zero(nums: Iterable[int]) -> int | float:
+    if not nums:
         return 0
 
-    if not isinstance(input_vals, Iterable):
-        raise TypeError(f"Input_vals parameter must be an iterable.")
+    if not isinstance(nums, Iterable):
+        raise TypeError("Input_vals must be an iterable.")
 
-    if isinstance(input_vals, Sequence) and len(input_vals) == 1:
-        return input_vals[0]
+    if isinstance(nums, Sequence) and len(nums) == 1:
+        return nums[0]
 
-    if isinstance(input_vals, Generator):
-        input_vals = list(input_vals)
+    if isinstance(nums, Generator):
+        nums = list(nums)
 
-    left_zero_vals = list(filter(lambda x: x < 0, input_vals))
+    left_zero_vals = list(filter(lambda x: x < 0, nums))
     min_left_val = -min(left_zero_vals) if left_zero_vals else math.inf
 
-    right_zero_vals = list(filter(lambda x: x >= 0, input_vals))
+    right_zero_vals = list(filter(lambda x: x >= 0, nums))
     min_right_val = min(right_zero_vals) if right_zero_vals else math.inf
 
     are_equal = min_right_val == min_left_val
