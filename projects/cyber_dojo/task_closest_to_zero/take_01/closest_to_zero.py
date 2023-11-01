855259c6b06ce31fac0b89547f8334dc42003eeb
japinol <japinol@outlook.com>
2023-11-01 21:05:58 +0100

    [IMP] cyber_dojo: Improve project: closest_to_zero.

--- projects/cyber_dojo/task_closest_to_zero/take_01/closest_to_zero.py
+++ projects/cyber_dojo/task_closest_to_zero/take_01/closest_to_zero.py
@@ -1,15 +1,15 @@
-from collections.abc import Iterable
-import math
+from typing import Iterable
+
+# We do not use math.inf because mypy complaints with a type error,
+# and we do not want to declare the return type of the function this way: int | float
+MAX_INT_VALUE: int = 99999999
 
 
-def get_number_closest_to_zero(nums: Iterable[int]) -> int | float:
+def get_number_closest_to_zero(nums: Iterable[int]) -> int:
     if not nums:
         return 0
 
-    if not isinstance(nums, Iterable):
-        raise TypeError("Input_vals must be an iterable.")
-
-    min_positive = min((x for x in nums if x >= 0), default=math.inf)
-    min_negative = min((x for x in nums if x < 0), default=-math.inf)
+    min_positive = min((x for x in nums if x >= 0), default=MAX_INT_VALUE)
+    min_negative = min((x for x in nums if x < 0), default=-MAX_INT_VALUE)
 
     return min_positive if min_positive < -min_negative else min_negative
--- projects/cyber_dojo/task_closest_to_zero/take_02/closest_to_zero.py
+++ projects/cyber_dojo/task_closest_to_zero/take_02/closest_to_zero.py
@@ -1,25 +1,18 @@
-from collections.abc import Generator, Iterable, Sequence
-import math
+from collections.abc import Iterable
+
+# We do not use math.inf because mypy complaints with a type error,
+# and we do not want to declare the return type of the function this way: int | float
+MAX_INT_VALUE: int = 99999999
 
 
-def get_number_closest_to_zero(nums: Iterable[int]) -> int | float:
+def get_number_closest_to_zero(nums: Iterable[int]) -> int:
     if not nums:
         return 0
 
     if not isinstance(nums, Iterable):
-        raise TypeError("Input_vals must be an iterable.")
+        raise TypeError(f"Input_vals must be an iterable instead of: {type(nums)}.")
 
-    if isinstance(nums, Sequence) and len(nums) == 1:
-        return nums[0]
+    min_positive = min(filter(lambda x: x >= 0, nums), default=MAX_INT_VALUE)
+    min_negative = min(filter(lambda x: x < 0, nums), default=-MAX_INT_VALUE)
 
-    if isinstance(nums, Generator):
-        nums = list(nums)
-
-    left_zero_vals = list(filter(lambda x: x < 0, nums))
-    min_left_val = -min(left_zero_vals) if left_zero_vals else math.inf
-
-    right_zero_vals = list(filter(lambda x: x >= 0, nums))
-    min_right_val = min(right_zero_vals) if right_zero_vals else math.inf
-
-    are_equal = min_right_val == min_left_val
-    return min_right_val if are_equal else min(min_left_val, min_right_val)
+    return min_positive if min_positive < -min_negative else min_negative
