5b37284e622800cd863b424c7cbe7fb789eabb13
japinol <japinol@outlook.com>
2024-03-05 20:40:51 +0100

    [IMP] gui_readiness_to_rock: Some improvements.

--- projects/various/src/gui_readiness_to_rock.py
+++ projects/various/src/gui_readiness_to_rock.py
@@ -1,10 +1,19 @@
+import logging
 from tkinter import messagebox
+
+logging.basicConfig(format='%(asctime)s %(levelname)s %(name)s: %(message)s')
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+
+QUESTION = "Are you ready to rock?"
 
 
 def main():
+    log.info("Starting app gui_readiness_to_rock")
+    log.info(f"Question: {QUESTION}")
     are_you_ready = messagebox.askyesno(
         title='Rock readiness',
-        message='Are you ready to rock?',
+        message=QUESTION,
         detail='Click Yes if you are awesome'
     )
 
@@ -13,7 +22,9 @@ def main():
     else:
         res = "Don't worry. You will be ready to rock soon enough."
 
-    print(res)
+    log.info(f"User answer: {'Yes' if are_you_ready else 'No'}")
+    log.info(f"Response: {res}")
+    log.info("End app")
 
 
 if __name__ == '__main__':
