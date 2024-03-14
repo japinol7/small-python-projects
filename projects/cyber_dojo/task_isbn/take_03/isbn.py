3a8188ca58cae09eb527d0e0d153bf0265564329
japinol <japinol@outlook.com>
2024-03-14 21:03:42 +0100

    [IMP] cyber_dojo: Add isbn solution with type hints.

---
+++ projects/cyber_dojo/task_isbn/take_03/isbn.py
@@ -0,0 +1,52 @@
+from collections.abc import Sequence
+
+
+def _validate_isbn_13(isbn_digits: Sequence, isbn_dc: str) -> str:
+    """Helper that validates if a given string is a valid ISBN-13 code."""
+    if not isbn_dc.isnumeric():
+        return 'false'
+
+    isbn_dc_num = int(isbn_dc)
+    # Multiply each digit alternately by 1 or 3 and sum these products together
+    check_digit = sum((
+            x * 3 if i % 2 == 0 else x for i, x in enumerate(isbn_digits, start=1)
+        ))
+    check_digit %= 10
+    check_digit = (10 - check_digit) % 10
+
+    if check_digit != isbn_dc_num:
+        return 'false'
+    return 'true'
+
+
+def _validate_isbn_10(isbn_digits: Sequence, isbn_dc: str) -> str:
+    """Validates if a given string is a valid ISBN-10 code."""
+    if not isbn_dc.isnumeric() and isbn_dc != 'X':
+        return 'false'
+
+    isbn_dc_num = int(isbn_dc) if isbn_dc != 'X' else 10
+
+    # Multiply each digit by its position number and sum these products together
+    check_digit = sum((
+            x * i for i, x in enumerate(isbn_digits, start=1)
+        ))
+    check_digit %= 11
+
+    if check_digit != isbn_dc_num:
+        return 'false'
+    return 'true'
+
+
+def validate_isbn(isbn: Sequence) -> str:
+    """Validates if a given string is a valid ISBN-13 or a valid ISBN-10 code."""
+    if any(not ch.isnumeric() and ch != '-' and ch != ' ' for ch in isbn[:-1]):
+        return 'false'
+
+    isbn_nums_without_dc = [int(ch) for ch in isbn[:-1] if ch.isnumeric()]
+    isbn_len = len(isbn_nums_without_dc) + 1
+
+    if isbn_len == 13:
+        return _validate_isbn_13(isbn_nums_without_dc, isbn[-1])
+    if isbn_len == 10:
+        return _validate_isbn_10(isbn_nums_without_dc, isbn[-1])
+    return 'false'
---
+++ projects/cyber_dojo/task_isbn/take_03/test_isbn.py
@@ -0,0 +1,44 @@
+from collections.abc import Sequence
+
+import pytest
+
+from isbn import validate_isbn
+
+
+@pytest.mark.parametrize('input_isbn, expected', [
+    ('9780470059029', 'true'),
+    ('978 0 471 48648 0', 'true'),
+    ('978-0596809485', 'true'),
+    ('978-0-13-149505-0', 'true'),
+    ('978-0-262-13472-9', 'true'),
+    ('978-0-262-13472-1', 'false'),
+    ('978-0-262-13472-2', 'false'),
+    ('978 0 A 471 48648 0', 'false'),
+    ('978 0 * 471 48648 0', 'false'),
+    ('978-0-262-13472-X', 'false'),
+    ('978-13472-2', 'false'),
+    ('978-0-A62-13472-1', 'false'),
+    ])
+def test_validate_isbn_13(input_isbn: Sequence, expected: str) -> None:
+    """Tests the validation of ISBN-13 codes."""
+    result = validate_isbn(input_isbn)
+    assert result == expected
+
+
+@pytest.mark.parametrize('input_isbn, expected', [
+    ('0471958697', 'true'),
+    ('0 471 60695 2', 'true'),
+    ('0-470-84525-2', 'true'),
+    ('0-321-14653-0', 'true'),
+    ('0-8044-2957-X', 'true'),
+    ('0-9752298-0-X', 'true'),
+    ('0-8044-2957-D', 'false'),
+    ('0-470-84525-3', 'false'),
+    ('0-4A0-84525-2', 'false'),
+    ('0-470-*4525-2', 'false'),
+    ('0-470-8425-2', 'false'),
+    ])
+def test_validate_isbn_10(input_isbn: Sequence, expected: str) -> None:
+    """Tests the validation of ISBN-10 codes."""
+    result = validate_isbn(input_isbn)
+    assert result == expected
