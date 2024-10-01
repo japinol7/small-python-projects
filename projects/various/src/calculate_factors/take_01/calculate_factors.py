c7ce9d504b01aaf98b8b9751fdf3105791e316d4
japinol <japinol@outlook.com>
2024-10-01 22:06:34 +0200

    [IMP] various: Add type hint in project calculate_factors.

--- projects/various/src/calculate_factors/take_01/calculate_factors.py
+++ projects/various/src/calculate_factors/take_01/calculate_factors.py
@@ -1,6 +1,6 @@
 from collections.abc import Generator
 
-def calculate_factors(n) -> Generator[int, None, None]:
+def calculate_factors(n: int) -> Generator[int, None, None]:
     for num in range(1, n + 1):
         if n % num == 0:
             yield num
