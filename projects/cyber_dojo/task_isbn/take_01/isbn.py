8cdf8852e1e9dafed02b1c0751dcfc89f8cac98a
japinol <japinol@outlook.com>
2025-03-31 23:19:06 +0200

    [IMP] cyber_dojo: Improve project: task_isbn.

--- projects/cyber_dojo/task_isbn/take_01/isbn.py
+++ projects/cyber_dojo/task_isbn/take_01/isbn.py
@@ -3,19 +3,18 @@ def validate_isbn(isbn):
     if any(not ch.isnumeric() and ch != '-' and ch != ' ' for ch in isbn):
         return 'false'
 
-    isbn_nums = [int(ch) for ch in isbn if ch.isnumeric()]
-    isbn_digits = isbn_nums[:-1]
-    isbn_control_digit_input = isbn_nums[-1]
-    if len(isbn_nums) != 13:
+    isbn = [int(ch) for ch in isbn if ch.isnumeric()]
+    if len(isbn) != 13:
         return 'false'
 
     # Multiply each digit alternately by 1 or 3 and sum these products together
-    check_digit = sum((
-            x * 3 if i % 2 == 0 else x for i, x in enumerate(isbn_digits, start=1)
-        ))
+    check_digit = sum(
+        x * 3 if i % 2 == 0 else x for i, x in enumerate(isbn[:-1], start=1)
+        )
     check_digit %= 10
     check_digit = (10 - check_digit) % 10
 
-    if check_digit != isbn_control_digit_input:
+    if check_digit != isbn[-1]:
         return 'false'
+
     return 'true'
